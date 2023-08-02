cf4d23627cc631472627a8c872c6304821315805
jooyyoo <aliceyun9@naver.com>
2023-08-02 11:15:37 +0900

    [feat] 2346 solved

---
+++ solution/data_structure/2346/jooyoung.py
@@ -0,0 +1,31 @@
+'''
+2346
+풍선 안에는 종이가 있다
+제일 처음은 1번 풍선을 터뜨리고
+풍선안에 있는 종이에 적혀있는 값만큼 이동하여 다음 풍선을 터뜨린다.
+양수가 적힌 경우에는 오른쪽으로, 음수가 적혀 있을 때는 왼쪽으로 이동
+'''
+N = int(input())
+balloon = [i + 1 for i in range(N)]  # 풍선
+number = list(map(int, input().split()))
+result = ""
+while True:
+    result += str(balloon.pop(0)) + " "
+    num = number.pop(0)  # 3
+
+    if len(balloon) == 1:
+        result += str(balloon.pop())
+        break
+
+    if num > 0:  # 양수라면
+        for _ in range(num - 1):
+            balloon.append(balloon.pop(0))
+            number.append(number.pop(0))
+    else:  # 음수라면
+        balloon.reverse()
+        num = (-num)
+        for _ in range(num - 1):
+            balloon.append(balloon.pop(0))
+            number.append(number.pop(0))
+
+print(result)
