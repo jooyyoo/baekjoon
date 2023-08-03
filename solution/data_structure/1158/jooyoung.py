3bcf5326efa69258f5d781128a24fb9fa8f1fa53
jooyyoo <aliceyun9@naver.com>
2023-08-03 11:31:37 +0900

    [feat] 1158 solved 백준 시간초과 해결

--- solution/data_structure/1158/jooyoung.py
+++ solution/data_structure/1158/jooyoung.py
@@ -1,12 +1,35 @@
+from collections import deque
+
 N, K = map(int, input().split())
 
-q = [i + 1 for i in range(N)]
 
-result = []
+def main():
+    q = [i + 1 for i in range(N)]
+    result = []
+    num = 0
+    for i in range(N):
+        num += K - 1  # 2
 
-while len(q) != 0:
-    for _ in range(K):
-        q.append(q.pop(0))
-    result.append(q.pop())
+        if num >= len(q):
+            num %= len(q)
 
-print('<' + ', '.join(map(str, result)) + '>')
+        result.append(str(q[num]))
+        q.pop(num)
+
+    print('<' + ', '.join(map(str, result)) + '>')
+
+
+def main2():
+    result = []
+    q = deque([i + 1 for i in range(N)])
+    while q:
+
+        for _ in range(K - 1):
+            q.append(q.popleft())
+
+        result.append(q.popleft())
+
+    print('<' + ', '.join(map(str, result)) + '>')
+
+
+main()
