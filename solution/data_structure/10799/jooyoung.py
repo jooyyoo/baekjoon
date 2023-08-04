223eb49113f1e0b664399b75022886a87835b312
jooyyoo <aliceyun9@naver.com>
2023-08-04 09:51:34 +0900

    [feat] 10799 solved

---
+++ solution/data_structure/10799/jooyoung.py
@@ -0,0 +1,21 @@
+'''
+10799
+( 이 나오면 스택에 추가 () 이 나오면 ( 의 개수를 세고 +
+) 이 나오면 1를 더해주고 스택에 있는 ( pop
+'''
+
+pipe = list(input())
+stack = []
+cnt = 0
+for i in range(len(pipe)):
+    if pipe[i] == '(':
+        stack.append('(')
+    else:  # ) 일때
+        if pipe[i - 1] == '(':  # '()' 라면
+            stack.pop()
+            cnt += len(stack)
+
+        else:  # 레이저의 끝 부분인 ( 라면
+            cnt += 1
+            stack.pop()
+print(cnt)
